6d4884c9e5abd228e32eadb425209596a4c2a4d2
IliyanVidev <iliyan.videv@industria.tech>
2024-08-19 16:10:53 +0300

    Empty output dir before building

--- scripts/build-dir.ps1
+++ scripts/build-dir.ps1
@@ -1,2 +1,6 @@
+# 1. Empty the output directory
+Remove-Item -Path dist\* -Force -Recurse
+# 2. Build the app
 pyinstaller src/main.py --onedir --noconfirm --name appraiser-geo
-Compress-Archive -Path dist/appraiser-geo -Destination dist/appraiser-geo -Force
+# 3. Compress the build output
+Compress-Archive -Path dist\appraiser-geo -Destination dist\appraiser-geo -Force
--- scripts/build-file.ps1
+++ scripts/build-file.ps1
@@ -1,2 +1,6 @@
+# 1. Empty the output directory
+Remove-Item -Path dist\* -Force -Recurse
+# 2. Build the app
 pyinstaller src/main.py --onefile --noconfirm --name appraiser-geo
-Compress-Archive -Path dist/appraiser-geo.exe -Destination dist/appraiser-geo -Force
+# 3. Compress the build output
+Compress-Archive -Path dist\appraiser-geo.exe -Destination dist\appraiser-geo -Force
