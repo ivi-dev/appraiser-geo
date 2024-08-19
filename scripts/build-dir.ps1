cc908ea43e9f884d5db371db37e2887c53331ee4
IliyanVidev <iliyan.videv@industria.tech>
2024-08-19 16:04:52 +0300

    Compress build output

--- scripts/build-dir.ps1
+++ scripts/build-dir.ps1
@@ -1,1 +1,2 @@
 pyinstaller src/main.py --onedir --noconfirm --name appraiser-geo
+Compress-Archive -Path dist/appraiser-geo -Destination dist/appraiser-geo -Force
--- scripts/build-file.ps1
+++ scripts/build-file.ps1
@@ -1,1 +1,2 @@
 pyinstaller src/main.py --onefile --noconfirm --name appraiser-geo
+Compress-Archive -Path dist/appraiser-geo.exe -Destination dist/appraiser-geo -Force
