76462d3af27bdea6fab10c63e582d0ee78334e6d
IliyanVidev <iliyan.videv@industria.tech>
2024-08-20 13:17:15 +0300

    Remove build output compression

--- scripts/build-dir.ps1
+++ scripts/build-dir.ps1
@@ -2,5 +2,3 @@
 Remove-Item -Path dist\* -Force -Recurse
 # 2. Build the app
 pyinstaller src/main.py --onedir --noconfirm --name appraiser-geo
-# 3. Compress the build output
-Compress-Archive -Path dist\appraiser-geo -Destination dist\appraiser-geo -Force
--- scripts/build-file.ps1
+++ scripts/build-file.ps1
@@ -2,5 +2,3 @@
 Remove-Item -Path dist\* -Force -Recurse
 # 2. Build the app
 pyinstaller src/main.py --onefile --noconfirm --name appraiser-geo
-# 3. Compress the build output
-Compress-Archive -Path dist\appraiser-geo.exe -Destination dist\appraiser-geo -Force
