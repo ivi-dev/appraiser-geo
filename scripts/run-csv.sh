971734db1f3cec4bf85dc996b3574bb37486be61
IliyanVidev <iliyan.videv@industria.tech>
2024-08-20 14:25:38 +0300

    Add Unix utiliy scripts

---
+++ scripts/run-csv.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/bash
+
+python -m src.main --csv "C:\Users\IliyanVidev\Projects\Mortgage eLending\Appraiser\geography_nomenclature_(ORIGINAL).csv" `
+                   --out "C:\Users\IliyanVidev\Desktop\Appraiser Resources\Градове-Форматирани[CSV].json"
---
+++ scripts/run-text.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+
+python -m src.main --cities "C:\Users\IliyanVidev\Desktop\Appraiser Resources\Градове.txt" `
+                   --neighborhoods "C:\Users\IliyanVidev\Desktop\Appraiser Resources\Квартали.txt" `
+                   --out "C:\Users\IliyanVidev\Desktop\Appraiser Resources\Градове-Форматирани[TEXT].json"
