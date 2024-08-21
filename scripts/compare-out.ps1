80a4ae2c5efc5266f6c45dd9233f763a7d0ace43
IliyanVidev <iliyan.videv@industria.tech>
2024-08-21 15:39:01 +0300

    Add program-output-compare utility script

---
+++ scripts/compare-out.ps1
@@ -0,0 +1,4 @@
+﻿$CsvOut = Get-Content '.\resources\Градове-Форматирани`[CSV`].json'
+$TextOut = Get-Content '.\resources\Градове-Форматирани`[TEXT`].json'
+
+Compare-Object $CsvOut $TextOut
---
+++ scripts/compare-out.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+
+CSV_OUT="./resources/Градове-Форматирани[CSV].json"
+TEXT_OUT="./resources/Градове-Форматирани[TEXT].json"
+
+diff $CSV_OUT $TEXT_OUT
