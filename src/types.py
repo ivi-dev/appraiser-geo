99674398b098d004bb9dd22433fd2e7702073ebf
IliyanVidev <iliyan.videv@industria.tech>
2025-02-13 10:15:09 +0200

    Add type definitions

--- src/types.py
+++ src/types.py
@@ -6,7 +6,34 @@ This module contains common type aliases.
 from typing import Iterable
 
 
+# A place's (city/town) geographical data, 
+# mainly its geography level 1 and 2
 Geo = dict[str, str]
+
+# A city's neighborhoods
 Neigborhoods = Iterable[str]
+
+# GeoJSON polygon data
+Polygons = tuple[tuple[float, float], ...]
+
+# A data structure mapping certain cities/town/neighborhoods
+# to their GeoJSON polygons
+RawPolygons = Iterable[dict[str, Geo | Polygons]]
+
+# A city's neighborhoods, enriched with their 
+# (GeoJSON) polygons data
+GeoNeigborhoods = Iterable[dict[str, Polygons]]
+
+# A city/town mapped to its geography level 1 and (possibly) 
+# 2 info only
 PartialCity = dict[str, Geo]
+
+# A city/town mapped to its geography level 1 and (possibly) 
+# 2 and neighborhoods
 City = dict[str, Geo | Neigborhoods]
+
+# A city/town mapped to its entire geo data - geography 
+# level 1 and (possibly) 2, neighborhoods and polygons.
+# Here, the city/town's neighborhoods are mapped to their 
+# own polygon data too.
+GeoCity = dict[str, Geo | GeoNeigborhoods | Polygons]
